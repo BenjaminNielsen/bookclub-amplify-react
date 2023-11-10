5e3f0f6708d992c963a4f0a68bb0afa7e07a24ee
Benjamin Nielsen <benjaminnielsen@carfax.com>
2023-11-10 17:26:10 -0500

    adds all social auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,10 @@ export type AmplifyDependentResourcesAttributes = {
     "bookclubamplifyreact7fc1a661": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
