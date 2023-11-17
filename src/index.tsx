204f2ac64b1b6b9c7c8215db7ce9f3c625a700b6
Benjamin Nielsen <benjaminnielsen@carfax.com>
2023-11-17 17:00:50 -0500

    modifies code to use aws-exports.js

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Amplify} from "aws-amplify";
-import amplifyconfig from './amplifyconfiguration.json';
+import amplifyConfig from './aws-exports.js';
 
-Amplify.configure(amplifyconfig);
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
