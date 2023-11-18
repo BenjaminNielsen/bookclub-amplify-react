a5d5f65500ecf166c794b0ae956e3c8784f27f96
Benjamin Nielsen <benjaminnielsen@carfax.com>
2023-11-18 14:32:52 -0500

    removes localhost

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Amplify} from "aws-amplify";
-import amplifyConfig from './aws-exports.js';
+import awsmobile from "./aws-exports";
 
-Amplify.configure(amplifyConfig);
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
