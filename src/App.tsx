dd9a3c67095de41d3ed390c6232266c77e595796
Benjamin Nielsen <benjaminnielsen@carfax.com>
2023-11-18 14:19:49 -0500

    rearranges the order of the redirectSignIn to be have the domain first

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,29 @@ import React from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import {
-    Authenticator,
     Tabs,
-    View,
+    View, withAuthenticator, WithAuthenticatorProps,
 } from "@aws-amplify/ui-react";
 import AddBook from "./components/AddBook/AddBook";
 import BookClubSuggestions from "./components/BookClubSuggestions/BookClubSuggestions";
 import MyBooks from "./components/MyBooks/MyBooks";
 import Header from "./components/Header/Header";
 
-class App extends React.Component {
-    render(): React.ReactElement | null {
-        return (
-            <Authenticator>
-                {({signOut, user}) => (
-                    <View className="App">
-                        <Header user={user} signOut={signOut}></Header>
-                        <Tabs justifyContent="center" defaultValue={'1'}
-                            items={[
-                            { label: 'My Books', value: '1', content: <MyBooks/> },
-                            { label: 'Book Club Ideas', value: '2', content: <BookClubSuggestions/> },
-                            { label: 'Add Book', value: '3', content: <AddBook/> },
-                        ]}
-                              isLazy={true}
-                        />
-                    </View>
-                )}
-            </Authenticator>
-        );
-    }
+
+export function App({signOut, user}: WithAuthenticatorProps) {
+    return (
+        <View className="App">
+            <Header user={user} signOut={signOut}></Header>
+            <Tabs justifyContent="center" defaultValue={'1'}
+                  items={[
+                      {label: 'My Books', value: '1', content: <MyBooks/>},
+                      {label: 'Book Club Ideas', value: '2', content: <BookClubSuggestions/>},
+                      {label: 'Add Book', value: '3', content: <AddBook/>},
+                  ]}
+                  isLazy={true}
+            />
+        </View>
+    )
 }
 
-export default App;
+export default withAuthenticator(App);
