19f504ec1e977608261703e7123addf380e693c9
Benjamin Nielsen <ben.a.nielsen@gmail.com>
2023-11-27 21:47:30 -0500

    Merges Main into add-ratings-values

--- src/components/MyBooks/UserBookDetails/UserBookDetails.tsx
+++ src/components/MyBooks/UserBookDetails/UserBookDetails.tsx
@@ -5,22 +5,22 @@ import {
     Button,
     Card,
     Divider,
-    Flex,
     Heading,
     Image,
     SliderField,
     TextField,
     View
 } from "@aws-amplify/ui-react";
-import { UserBooks} from "../../../types/API";
+import {UserBooks} from "../../../types/API";
 import {Form, Link, useLoaderData} from "react-router-dom";
 
 
 export default function UserBookDetails(): React.ReactElement | null {
 
-    const userBook:UserBooks  = useLoaderData() as UserBooks;
+    const userBook: UserBooks = useLoaderData() as UserBooks;
     const [dateStarted, setDateStarted] = useState(userBook.dateStarted)
     const [dateFinished, setDateFinished] = useState(userBook.dateFinished)
+    const [percentComplete, setPercentComplete] = useState(userBook.progress ?? 0)
 
     function onDateStartedChanged(e: React.ChangeEvent<HTMLInputElement>): void {
         const newDate: string = e.target.value
@@ -32,40 +32,7 @@ export default function UserBookDetails(): React.ReactElement | null {
         setDateFinished(newDate)
     }
 
-    // function onRatingsClick(): void {
-    //     const navigate = useNavigate();
-    //     navigate(`ratings/${userBook.id}`, {relative: "path"})
-    // }
-
-    // async function onUpdateClick() {
-    //     if (!userBook.id) {
-    //         console.error("tried to edit details on book with no id")
-    //         return
-    //     }
-    //     await API.graphql({
-    //         query: updateUserBooks,
-    //         variables: {
-    //             input: {
-    //                 "id": userBook.id,
-    //                 "isbn": userBook.isbn,
-    //                 "title": userBook.title,
-    //                 "author": ['Ben'],
-    //                 "genre": ['Genre'],
-    //                 "numberInSeries": userBook.numberInSeries,
-    //                 "wordCount": userBook.wordCount,
-    //                 "description": userBook.description,
-    //                 "progress": 1020,
-    //                 "dateStarted": dateStarted,
-    //                 "dateFinished": dateFinished
-    //             }
-    //         },
-    //     });
-    //     onUpdateParent()
-    // }
-
-
     return (
-        <Flex justifyContent="center">
             <Card
                 borderRadius="medium"
                 variation="outlined"
@@ -80,17 +47,18 @@ export default function UserBookDetails(): React.ReactElement | null {
                     >
                         <Divider padding="xs"/>
                         <Heading className="bookTitle" padding="medium">{userBook.title}</Heading>
-                        <input type="hidden" name="id" value={userBook?.id??""}/>
-                        <input type="hidden" name="isbn" value={userBook?.isbn??""}/>
-                        <input type="hidden" name="title" value={userBook?.title??""}/>
-                        <input type="hidden" name="thumbnailUrl" value={userBook?.thumbnailUrl??""}/>
-                        <input type="hidden" name="author" value={userBook?.author?.map((author)=>author??"")}/>
-                        <input type="hidden" name="genre" value={userBook?.genre?.map(genre => genre??"")}/>
-                        <input type="hidden" name="numberInSeries" value={userBook?.numberInSeries??""}/>
-                        <input type="hidden" name="wordCount" value={userBook?.wordCount??""}/>
-                        <input type="hidden" name="description" value={userBook?.description??""}/>
+                        <input type="hidden" name="id" value={userBook?.id ?? ""}/>
+                        <input type="hidden" name="isbn" value={userBook?.isbn ?? ""}/>
+                        <input type="hidden" name="title" value={userBook?.title ?? ""}/>
+                        <input type="hidden" name="thumbnailUrl" value={userBook?.thumbnailUrl ?? ""}/>
+                        <input type="hidden" name="author" value={userBook?.author?.map((author) => author ?? "")}/>
+                        <input type="hidden" name="genre" value={userBook?.genre?.map(genre => genre ?? "")}/>
+                        <input type="hidden" name="numberInSeries" value={userBook?.numberInSeries ?? ""}/>
+                        <input type="hidden" name="wordCount" value={userBook?.wordCount ?? ""}/>
+                        <input type="hidden" name="description" value={userBook?.description ?? ""}/>
 
-                        <SliderField name="progress" label="Percent Complete" value={percentComplete} onChange={setPercentComplete} min={0} max={100} step={1}/>
+                        <SliderField name="progress" label="Percent Complete" value={percentComplete}
+                                     onChange={setPercentComplete} min={0} max={100} step={1}/>
 
                         <TextField name="dateStarted"
                                    label={"Date Started"}
@@ -104,34 +72,28 @@ export default function UserBookDetails(): React.ReactElement | null {
                                    onChange={onDateFinishedChanged}/>
                         <div>
                             {dateFinished && <Link to={`rating/${userBook.id}`}>
-                        <Button type="button" variation="primary" className="ratingsButton">Ratings</Button>
-                        </Link>}
+                                <Button type="button" variation="primary" className="ratingsButton">Ratings</Button>
+                            </Link>}
                         </div>
-                            <Button type="submit" colorTheme="info">Update</Button>
-
-                    <ButtonGroup justifyContent="center" variation="primary">
-                        <Form
-                            method="post"
-                            action="destroy"
-                            onSubmit={(event) => {
-                                if (
-                                    !window.confirm(
-                                        "Please confirm you want to delete this record."
-                                    )
-                                ) {
-                                    event.preventDefault();
-                                }
-                            }}
-                        >
-                            <Button className="deleteButton" type="submit" colorTheme="error">Delete</Button>
-                        </Form>
-                        <Button variation="primary" className="updateButton"> Update </Button>
-                    </ButtonGroup>
-
-
+                        <Button type="submit" colorTheme="info">Update</Button>
+                    </Form>
+                    <Form
+                        method="post"
+                        action="destroy"
+                        onSubmit={(event) => {
+                            if (
+                                !window.confirm(
+                                    "Please confirm you want to delete this record."
+                                )
+                            ) {
+                                event.preventDefault();
+                            }
+                        }}
+                    >
+                        <Button className="deleteButton" type="submit" colorTheme="error">Delete</Button>
+                    </Form>
                 </View>
             </Card>
-        </Flex>
     )
 
 }
